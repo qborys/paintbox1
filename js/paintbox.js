be4191d4c8bbddc0c5a710e459cfb19ec22764d9
Piotr Borychowski <qborys@gmail.com>
2019-11-13 20:48:59 +0100

    działający przycisk toggle czytaj wiecej mniej

--- js/paintbox.js
+++ js/paintbox.js
@@ -1,4 +1,9 @@
 function myFunction() {
-    var element = document.getElementById("read_more");
-    element.classList.toggle("hide");
+    var readmore = document.getElementById("read_more");
+    readmore.classList.toggle("hide");
+    var wiecej = document.getElementById("wiecej");
+    wiecej.classList.toggle("hide");
+    var mniej = document.getElementById("mniej");
+    mniej.classList.toggle("hide");
+
 }
