8cebc3dd7522bdb780488334343caf60a3cf904b
Piotr Borychowski <qborys@gmail.com>
2019-11-15 21:25:44 +0100

    dotyk działa

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -84,6 +84,23 @@ if (typeof jQuery === 'undefined') {
  * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
  * ======================================================================== */
 
+$(".carousel").on("touchstart", function (event) {
+    var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function (event) {
+        var xMove = event.originalEvent.touches[0].pageX;
+        if (Math.floor(xClick - xMove) > 5) {
+            $(this).carousel('next');
+        }
+        else if (Math.floor(xClick - xMove) < -5) {
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function () {
+        $(this).off("touchmove");
+    });
+});
+
+
 
 +function ($) {
     'use strict';
@@ -535,60 +552,7 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
-(function ($) {
-    $.fn.bcSwipe = function (settings) {
-        var config = { threshold: 50 };
-        if (settings) {
-            $.extend(config, settings);
-        }
 
-        this.each(function () {
-            var stillMoving = false;
-            var start;
-
-            if ('ontouchstart' in document.documentElement) {
-                this.addEventListener('touchstart', onTouchStart, false);
-            }
-
-            function onTouchStart(e) {
-                if (e.touches.length == 1) {
-                    start = e.touches[0].pageX;
-                    stillMoving = true;
-                    this.addEventListener('touchmove', onTouchMove, false);
-                }
-            }
-
-            function onTouchMove(e) {
-                if (stillMoving) {
-                    var x = e.touches[0].pageX;
-                    var difference = start - x;
-                    if (Math.abs(difference) >= config.threshold) {
-                        cancelTouch();
-                        if (isCarouselEmpty($(this))) return;
-                        if (difference > 0) {
-                            $(this).carousel('next');
-                        }
-                        else {
-                            $(this).carousel('prev');
-                        }
-                    }
-                }
-            }
-
-            function isCarouselEmpty(carousel) {
-                return carousel.find(".item.active").length < 1
-            }
-
-            function cancelTouch() {
-                this.removeEventListener('touchmove', onTouchMove);
-                start = null;
-                stillMoving = false;
-            }
-        });
-
-        return this;
-    };
-})(jQuery);
 
 /* ========================================================================
  * Bootstrap: collapse.js v3.3.2
