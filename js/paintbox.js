a300f7938dfeebfb517e930166081933d3ccdece
Piotr Borychowski <qborys@gmail.com>
2019-12-08 02:17:18 +0100

    przyciski

--- js/paintbox.js
+++ js/paintbox.js
@@ -32,6 +32,39 @@ function wylaczmenu() {
 
 
 
+// var slideIndex = 1;
+// showSlides(slideIndex);
+
+// // Next/previous controls
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+// // Thumbnail image controls
+// function currentSlide(n) {
+//     showSlides(slideIndex = n);
+// }
+
+// function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var fotka = document.getElementsByClassName("fotka");
+//     var dots = document.getElementsByClassName("demo");
+//     var captionText = document.getElementById("caption");
+//     if (n > slides.length) { slideIndex = 1 }
+//     if (n < 1) { slideIndex = slides.length }
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].classList.add("mySlides_hover");
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex - 1].classList.remove("mySlides_hover");
+//     dots[slideIndex - 1].className += " active";
+//     captionText.innerHTML = dots[slideIndex - 1].alt;
+// }
+
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -67,6 +100,8 @@ function showSlides(n) {
 
 
 
+
+
 $(document).ready(function () {
     $("form").submit(function (event) {
         $.post("form.php", $(this).serialize(), function (data) {
@@ -76,3 +111,5 @@ $(document).ready(function () {
     });
 
 });
+
+
