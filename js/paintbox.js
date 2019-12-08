e70df5559973fb4a47da2c6aff237f27535d86e8
Piotr Borychowski <qborys@gmail.com>
2019-12-08 15:26:46 +0100

    poprawka błędu przewijania carousel

--- js/paintbox.js
+++ js/paintbox.js
@@ -1,20 +1,3 @@
-function myFunction() {
-    var readmore = document.getElementById("read_more");
-    readmore.classList.toggle("hide");
-    var wiecej = document.getElementById("wiecej");
-    wiecej.classList.toggle("hide");
-    var mniej = document.getElementById("mniej");
-    mniej.classList.toggle("hide");
-}
-
-
-function zmianatla() {
-    var guzik = document.getElementById("guzik");
-    guzik.classList.toggle("bg_cherry");
-
-}
-
-
 function umowwizyte() {
     var popup = document.getElementById("popup");
     popup.classList.toggle("hide");
@@ -27,43 +10,15 @@ function wylaczmenu() {
 
 }
 
+$(document).ready(function () {
+    $("form").submit(function (event) {
+        $.post("form.php", $(this).serialize(), function (data) {
+            alert(data);
+        });
+        event.preventDefault();
+    });
 
-
-
-
-
-// var slideIndex = 1;
-// showSlides(slideIndex);
-
-// // Next/previous controls
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-
-// // Thumbnail image controls
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     var fotka = document.getElementsByClassName("fotka");
-//     var dots = document.getElementsByClassName("demo");
-//     var captionText = document.getElementById("caption");
-//     if (n > slides.length) { slideIndex = 1 }
-//     if (n < 1) { slideIndex = slides.length }
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].classList.add("mySlides_hover");
-//     }
-//     for (i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" active", "");
-//     }
-//     slides[slideIndex - 1].classList.remove("mySlides_hover");
-//     dots[slideIndex - 1].className += " active";
-//     captionText.innerHTML = dots[slideIndex - 1].alt;
-// }
-
+});
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -95,21 +50,3 @@ function showSlides(n) {
     dots[slideIndex - 1].className += " active";
     captionText.innerHTML = dots[slideIndex - 1].alt;
 }
-
-
-
-
-
-
-
-$(document).ready(function () {
-    $("form").submit(function (event) {
-        $.post("form.php", $(this).serialize(), function (data) {
-            alert(data);
-        });
-        event.preventDefault();
-    });
-
-});
-
-
